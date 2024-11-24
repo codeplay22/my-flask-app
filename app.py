e393ee67ed6cdb8887538c7ca2fd050e8ab10438
codeplay22 <anshuman.khaneja@gmail.com>
2024-11-24 13:14:41 +0530

    modified some files

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'GFGGGGGG1234'
+        return 'hellothere'
 
     return app
 
--- test_app.py
+++ test_app.py
@@ -15,5 +15,5 @@ def client(app):
 def test_home(client):
     response = client.get('/') 
     assert response.status_code == 200
-    expected_text = 'GFGGGGGG1234'
+    expected_text = 'hellothere'
     assert expected_text.encode() in response.data
